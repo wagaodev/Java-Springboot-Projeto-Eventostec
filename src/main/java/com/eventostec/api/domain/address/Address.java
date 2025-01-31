d32e7e63493f9ea3bbbf6fcee779e6cf71f40cdb
Wagner Goulart <wagner.goulart.3@globalhitss.com.br>
2025-01-31 02:44:26 -0300

    feat: entity created

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,24 @@
+package com.eventostec.api.domain.address;
+
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
---
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -0,0 +1,32 @@
+package com.eventostec.api.domain.coupon;
+
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
