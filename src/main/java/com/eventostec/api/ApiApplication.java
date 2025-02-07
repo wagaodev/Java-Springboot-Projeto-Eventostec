5924cab431296faddae6ae6509d232e1dab3b2be
Wagner Goulart <wagner.goulart.3@globalhitss.com.br>
2025-02-07 02:22:07 -0300

    chore: default structure and libs added

--- src/main/java/com/eventostec/api/ApiApplication.java
+++ src/main/java/com/eventostec/api/ApiApplication.java
@@ -3,11 +3,19 @@ package com.eventostec.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
+		System.setProperty("AWS_BUCKET_NAME", dotenv.get("AWS_BUCKET_NAME"));
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
+
